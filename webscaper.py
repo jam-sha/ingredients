3e902135afd47739c53550e7329877a0755cbb60
James Shapas <j@jmba.local>
2022-07-17 22:37:39 -0400

    made scraper

--- webscaper.py
+++ webscaper.py
@@ -1,14 +1,42 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait     
+from selenium.webdriver.common.by import By     
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.implicitly_wait(10) # this lets webdriver wait 10 seconds for the website to load
-driver.get("https://www.ewg.org/skindeep/")
+#driver.get("https://www.ewg.org/skindeep/")
+
 
 barcodes = ["Dimethiconol"]
 for barcode in barcodes:
-    text_box = driver.find_element_by_css_selector('eac-item') # input selector
-    text_box.send_keys(barcode) # enter text in input
+ #   text_box = driver.find_element(By.CLASS_NAME, "homepage-search-input")
+  #  text_box.send_keys(barcode)
 
-    driver.find_element_by_xpath('//*[@id="eac-container-search"]/ul/li/div').click() # click the submit button
 
+  #  elems = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "eac-item [href]")))
+
+  #  itemData = elems[0].get_attribute('href') # link of item were serching
+    itemLink = "https://www.ewg.org/skindeep/ingredients/702044-dimethiconol"
+
+    driver.get(itemLink)
+
+    about = driver.find_element(By.XPATH, "/html/body/div[2]/div/main/section[1]/div[1]/div[3]/section/p").get_attribute('innerHTML')
+    #item info
+
+    dataAvailability = driver.find_element(By.XPATH, "/html/body/div[2]/div/main/section[1]/div[1]/div[3]/div[1]/div[1]/p").get_attribute('innerHTML')
+    #data level
+
+    
+ 
+                                        
+   
+
+
+time.sleep(1000)
 driver.quit()   
